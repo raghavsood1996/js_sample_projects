6d9ea13837f98247a3e2199b394ac50411aa1b12
raghavsood1996 <raghavsood1996@github.com>
2021-12-05 22:07:39 -0500

    added file watching project

---
+++ watchit/index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+const chokidar = require("chokidar");
+const debounce = require("lodash.debounce");
+const program = require("caporal");
+const fs = require("fs");
+const { spawn } = require("child_process");
+const chalk = require("chalk");
+
+program
+  .version("1.0.0")
+  .argument("[filename]", "Name of a file to execute")
+  .action(async ({ filename }) => {
+    // debouncing the function so that add event is not triggered
+    // so often
+    const name = filename || "index.js";
+
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`);
+    }
+
+    let proc;
+    const start = debounce(() => {
+      if (proc) {
+        proc.kill();
+      }
+      console.log(chalk.green(">>>> Starting Process..."));
+      proc = spawn("node", [name], { stdio: "inherit" });
+    }, 500);
+
+    chokidar
+      .watch(".")
+      .on("add", start)
+      .on("change", start)
+      .on("unlink", start);
+  });
+
+program.parse(process.argv);
